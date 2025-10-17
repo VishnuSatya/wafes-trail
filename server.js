fa63c07cf3f93bab1ac673f3be857f5b0a8295c5
VishnuSatya <vishnusatya9596@hotmail.com>
2025-10-17 13:59:24 -0400

    change in mongo

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB (set MONGO_URI in .env)
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true ,ssl:true})
   .then(()=> console.log('MongoDB connected'))
   .catch(err=> console.error(err));
 
@@ -25,5 +25,5 @@ app.use('/api/auth', authRoutes);      // existing
 app.use('/api/videos', videoRoutes);
 
 // start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
